5bc6a0489ac2242505ca872204a277fff97afc1f
matheus_aragao <matheusaragaofs@gmail.com>
2022-11-16 22:09:51 -0300

    adding video

--- src/container/Intro/Intro.tsx
+++ src/container/Intro/Intro.tsx
@@ -1,11 +1,43 @@
 import React from 'react';
 
 import './Intro.css';
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+import { meal } from '../../constants';
+import './Intro.css';
+const Intro = () => {
+  const [playVideo, setPlayVideo] = React.useState(false);
+  const vidRef = React.useRef({} as any);
 
+  return (
+    <div className="app__video">
+      <video
+        ref={vidRef as any}
+        src={meal}
+        loop
+        controls={false}
+        muted
+      />
+      <div className="app__video-overlay flex__center">
+        <div
+          className="app__video-overlay_circle flex__center"
+          onClick={() => {
+            setPlayVideo(!playVideo);
+            if (playVideo) {
+              vidRef?.current.pause();
+            } else {
+              vidRef?.current.play();
+            }
+          }}
+        >
+          {playVideo ? (
+            <BsPauseFill color="#fff" fontSize={30} />
+          ) : (
+            <BsFillPlayFill color="#fff" fontSize={30} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Intro;
